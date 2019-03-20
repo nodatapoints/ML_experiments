1feb2a2da9f8253200ef123357e0fc697cd86fc5
nodatapoints <nodatapoints@gmail.com>
2019-03-20 17:28:33 +0100

    overwrite circles.py with improved version

--- GAN/circles.py
+++ GAN/circles.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from __future__ import print_function
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,7 +15,9 @@ from keras.layers import Input, \
                          BatchNormalization, \
                          Dropout, \
                          Conv2D, \
-                         MaxPool2D
+                         MaxPool2D, \
+                         UpSampling2D
+
 # from keras.layers.advanced_activations import LeakyReLU
 # from keras.optimizers import Adam
 
@@ -36,42 +37,44 @@ images = images / 127 - 1
 model = Sequential()
 
 model.add(Dense(32, activation='relu'))
-model.add(Dense(512, activation='relu'))
-# model.add(BatchNormalization(momentum=.9))
+# model.add(Dense(512, activation='relu'))
 model.add(Dropout(.5))
-model.add(Dense(512, activation='relu'))
-# model.add(BatchNormalization(momentum=.9))
+model.add(BatchNormalization())
+model.add(Dense(64*10*10, activation='relu'))
+model.add(BatchNormalization())
+model.add(Reshape((10, 10, 64)))
 model.add(Dropout(.5))
-# model.add(Dense(1024, activation='relu'))
-# model.add(BatchNormalization(momentum=.9))
-# model.add(Dropout(.4))
-model.add(Dense(np.prod(image_dims), activation='tanh'))
+model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(UpSampling2D((2, 2)))
+model.add(Conv2D(16, (3, 3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(1, (3, 3), padding='same', activation='tanh'))
 model.add(Reshape(image_dims))
 
 noise = Input(shape=(noise_dims, ))
 image_out = model(noise)
 
 generator = Model(noise, image_out)
-
+generator.summary()
 
 # Build discriminator
 
 model = Sequential()
 
-# model.add(Reshape(image_dims+(1,), input_shape=image_dims))
-# model.add(Conv2D(64, (3, 3), activation='relu'))
-# model.add(MaxPool2D((2, 2)))
+model.add(Reshape(image_dims+(1,), input_shape=image_dims))
+model.add(Conv2D(16, (3, 3), activation='relu'))
+model.add(MaxPool2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(Flatten())
-model.add(Dense(256, activation='relu'))
-model.add(Dropout(.1))
-model.add(Dense(32, activation='relu'))
-model.add(Dropout(.4))
+# model.add(Dense(256, activation='relu'))
 model.add(Dense(32, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 image_in = Input(shape=image_dims)
 validity = model(image_in)
 discriminator = Model(image_in, validity)
+discriminator.summary()
 
 discriminator.compile(
     optimizer='Adam',
@@ -109,8 +112,7 @@ def plot_samples(history):
     for epoch_i, samples in enumerate(history):
         for sample_i, sample in enumerate(samples):
             plot_image(sample)
-            # plt.savefig(f'samples/{epoch_i:03.0f}_{sample_i:02.0f}.png')  # Python 3
-            plt.savefig('samples/%03.0f_%02.0f.png' % (epoch_i, sample_i))
+            plt.savefig(f'samples/{epoch_i:03.0f}_{sample_i:02.0f}.png')
 
 
 def train_generator(batch_size=32):
@@ -118,6 +120,25 @@ def train_generator(batch_size=32):
     real = np.ones((batch_size, 1))
 
     return stacked.train_on_batch(noise, real)
+
+
+def plot_stats(stats):
+    stacked_loss, loss_fake, loss_real, acc_fake, acc_real = zip(*stats)
+
+    fig, (ax_loss, ax_acc) = plt.subplots(2, 1)
+
+    ax_loss.plot(stacked_loss)
+    ax_loss.plot(loss_real)
+    ax_loss.plot(loss_fake)
+    ax_acc.plot(acc_fake)
+    ax_acc.plot(acc_real)
+
+    ax_loss.set_ylabel('loss')
+    ax_loss.legend(('stacked', 'd real', 'd fake'))
+    ax_acc.set_ylabel('accuracy')
+    ax_acc.legend(('fake', 'real'))
+
+    fig.savefig('stats.png')
 
 
 def train_discriminator(batch_size=32):
@@ -136,41 +157,25 @@ def train_discriminator(batch_size=32):
     return loss_fake, loss_real, acc_fake, acc_real
 
 
-def plot_stats(stats):
-    loss, acc_fake, acc_real = zip(*stats)
-
-    fig, (ax_loss, ax_acc) = plt.subplots(2, 1)
-
-    ax_loss.plot(loss)
-    ax_acc.plot(acc_fake)
-    ax_acc.plot(acc_real)
-
-    ax_loss.legend(('loss',))
-    ax_acc.legend(('fake', 'real'))
-
-    fig.savefig('stats.png')
-
 # misc container for stats over all epochs
 stats = []
 
 # list of lists containing `n_samples` of random images for every 100th epoch
 history = []
 
-n_samples = 1
-epoch = 1
+n_samples = 5
 
-while True:
+for epoch in range(900):
     try:
         stacked_loss = train_generator()
-        _, _, acc_fake, acc_real = train_discriminator()
+        loss_fake, loss_real, acc_fake, acc_real = train_discriminator()
 
         # stats for later evaluation
-        stats.append((stacked_loss, acc_fake, acc_real))
+        stats.append((stacked_loss, loss_fake, loss_real, acc_fake, acc_real))
 
         # Take samples every 100 epochs
         if epoch % 100 == 0:
-            # print(f'epoch {epoch:10}    loss stacked: {stacked_loss:2.3f}    acc fake: {acc_fake:0.5f}    acc real: {acc_real:0.5f}')  # Python 3
-            print('epoch %04d    loss stacked: %2.3f    acc fake: %.5f    acc real: %.5f' % (epoch, stacked_loss, acc_fake, acc_real) )
+            print(f'epoch {epoch:10}    loss stacked: {stacked_loss:2.3f}    acc fake: {acc_fake:0.5f}    acc real: {acc_real:0.5f}')
 
             samples = generator.predict(
                 np.random.normal(0, 1, (n_samples, noise_dims)))
@@ -179,7 +184,7 @@ while True:
         epoch += 1
 
     except KeyboardInterrupt:
-        print('Aborted. Total epochs:', epoch)
+        print(f'Aborted. Total epochs: {epoch}')
         break
 
 plot_samples(history)
