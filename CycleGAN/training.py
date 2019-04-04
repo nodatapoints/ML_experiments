d8b86f461d0e5e7919e0b7e2db05194abfd7e3d8
nodatapoints <nodatapoints@gmail.com>
2019-04-04 11:13:38 +0200

    integrate sample collection

--- CycleGAN/training.py
+++ CycleGAN/training.py
@@ -2,13 +2,15 @@ from collections import namedtuple
 
 
 class Training:
-    def __init__(self, n_generations: int, log_period: int=100, stat_entries: tuple=()):
+    def __init__(self, n_generations: int, log_period: int=100, sample_period: int=100, stat_entries: tuple=()):
         self.n_generations = n_generations
         self.log_period = log_period
+        self.sample_period = sample_period
         self.stat_class = namedtuple('Stats', ('x', ) + stat_entries)
         self.gen = 0
         self._stat_list = []
         self.stats = None
+        self.samples = []
 
     def __iter__(self):
         return self._train()
@@ -18,14 +20,19 @@ class Training:
             self.gen = i
             yield
 
-        self._compile_stats()
-
     @property
     def log(self):
         return self.gen % self.log_period == 0
 
+    @property
+    def make_samples(self):
+        return self.gen % self.sample_period == 0
+
     def append_stats(self, *args):
         self._stat_list.append((self.gen, ) + args)
 
-    def _compile_stats(self):
+    def append_sample(self, sample, name: str):
+        self.samples.append((sample, name))
+
+    def compile_records(self):
         self.stats = self.stat_class(*zip(*self._stat_list))
